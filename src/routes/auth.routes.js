96fef6a663feaaf8e5db9e2384e10516308098ae
kumaradityaapril <kumaraditya.bvn@gmail.com>
2026-07-01 11:42:03 +0530

    Applied logout feature

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,7 +3,9 @@ const authController = require("../controllers/auth.controller")
 
 const authRouter = express.Router()
 
-authRouter.post("/register",authController.registerUserController)
-authRouter.post("/login",authController.loginUserController)
+authRouter.post("/register", authController.registerUserController)
+authRouter.post("/login", authController.loginUserController)
+
+authRouter.get("/logout",authController.logoutUserController)
 
 module.exports = authRouter
