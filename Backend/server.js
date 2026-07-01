649ce8c0f6a43a806390b775e7627b81625ac9dd
kumaradityaapril <kumaraditya.bvn@gmail.com>
2026-07-01 13:16:44 +0530

    Minor update in backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
-const app = require("../src/app")
-const connectToDB = require("../src/config/database")
+const app = require("./src/app")
+const connectToDB = require("./src/config/database")
 const dns = require("dns")
 
 dns.setServers(["1.1.1.1","8.8.8.8"])
